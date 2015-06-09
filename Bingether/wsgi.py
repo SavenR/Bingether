4a4b9ec03f710c5afd203806e3ba2429688f8991
Saven Roybal <SavenR@users.noreply.github.com>
2015-06-09 14:53:20 -0700

    Updated project name

--- Bingether/wsgi.py
+++ Bingether/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "django_drf_starter_project.settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "Bingether")
 
 application = get_wsgi_application()
