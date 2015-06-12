a777a7e8554cac316eba7aa3205d15cea038a4c5
Saven Roybal <SavenR@users.noreply.github.com>
2015-06-12 16:26:46 -0700

    Using a less hacky way to update title and page css dynamically. Fixes #50

--- jsframework/static/jsframework/js/ng/apps/app.js
+++ jsframework/static/jsframework/js/ng/apps/app.js
@@ -2,7 +2,6 @@
 var app = angular.module( "app", [ 'ngRoute' ] );
 
 app.config( [ '$routeProvider', function( $routeProvider ){
-    console.log('in app.config');
     $routeProvider
     .when( '/search', {
         templateUrl: '/static/jsframework/views/search.html',
@@ -14,37 +13,37 @@ app.config( [ '$routeProvider', function( $routeProvider ){
         templateUrl: '/static/jsframework/views/bingeList.html',
         controller: 'mainController',
         title: 'Binges to Be',
-        css: 'static/base/css/test.css'
+        css: 'static/app/css/bingeList.css'
      } )
     .when( '/community', {
         templateUrl: '/static/jsframework/views/communityBRs.html',
         controller: 'mainController',
         title: 'Community Bingether Requests',
-        css: 'static/base/css/test.css'
+        css: 'static/app/css/communityBRs.css'
      } )
     .when( '/my_bingethers', {
         templateUrl: '/static/jsframework/views/myBingethers.html',
         controller: 'mainController',
         title: 'My Bingethers',
-        css: 'static/base/css/test.css'
+        css: 'static/app/css/myBingethers.css'
      } )
     .when( '/log_out', {
         templateUrl: '/static/jsframework/views/logOut.html',
         controller: 'mainController',
         title: 'Take care!',
-        css: 'static/base/css/test.css'
+        css: 'static/app/css/logOut.css'
     })
     .when( '/', {
         templateUrl: '/static/jsframework/views/index.html',
         controller: 'mainController',
         title: 'Welcome',
-        css: 'static/base/css/test.css'
+        css: 'static/app/css/index.css'
     })
     .when( '/uh_oh', {
         templateUrl: '/static/jsframework/views/uhOh.html',
         controller: 'mainController',
         title: 'Oh no...',
-        css: 'static/base/css/test.css'
+        css: 'static/app/css/uhOh.css'
     })
     .otherwise({
         redirectTo: '/uh_oh'
@@ -54,9 +53,10 @@ app.config( [ '$routeProvider', function( $routeProvider ){
 app.run( [ '$location', '$rootScope', function( $location, $rootScope ){
     $rootScope.$on( '$routeChangeSuccess', function( event, current, previous ){
         if ( current.$$route ){
-            $rootScope.title = current.$$route.title;
-            $rootScope.css = current.$$route.css;
+            document.getElementById('pageTitle').innerHTML = current.$$route.title;
+            document.getElementById('pageCSS').setAttribute('href', current.$$route.css);
         }
+
     } )
 }
 ]);
