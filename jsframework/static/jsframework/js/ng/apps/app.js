9099fb7f1635d63addf6934e192d106575367cdb
Saven Roybal <SavenR@users.noreply.github.com>
2015-06-11 19:04:09 -0700

    Connected all routes to a standin css for testing.

--- jsframework/static/jsframework/js/ng/apps/app.js
+++ jsframework/static/jsframework/js/ng/apps/app.js
@@ -7,27 +7,32 @@ app.config(['$routeProvider', function($routeProvider){
     .when( '/search', {
         templateUrl: '/static/jsframework/views/search.html',
         controller: 'mainController',
-        title: 'Search Page'
+        title: 'Search Page',
+        css: 'static/base/css/test.css'
      } )
     .when( '/binge_list', {
         templateUrl: '/static/jsframework/views/bingeList.html',
         controller: 'mainController',
-        title: 'Binges to Be'
+        title: 'Binges to Be',
+        css: 'static/base/css/test.css'
      } )
     .when( '/community', {
         templateUrl: '/static/jsframework/views/communityBRs.html',
         controller: 'mainController',
-        title: 'Community Bingether Requests'
+        title: 'Community Bingether Requests',
+        css: 'static/base/css/test.css'
      } )
     .when( '/my_bingethers', {
         templateUrl: '/static/jsframework/views/myBingethers.html',
         controller: 'mainController',
-        title: 'My Bingethers'
+        title: 'My Bingethers',
+        css: 'static/base/css/test.css'
      } )
     .when('/log_out', {
         templateUrl: '/static/jsframework/views/logOut.html',
         controller: 'mainController',
-        title: 'Take care!'
+        title: 'Take care!',
+        css: 'static/base/css/test.css'
     })
     .when('/', {
         templateUrl: '/static/jsframework/views/index.html',
@@ -38,7 +43,8 @@ app.config(['$routeProvider', function($routeProvider){
     .when('/uh_oh', {
         templateUrl: '/static/jsframework/views/uhOh.html',
         controller: 'mainController',
-        title: 'Oh no...'
+        title: 'Oh no...',
+        css: 'static/base/css/test.css'
     })
     .otherwise({
         redirectTo: '/uh_oh'
