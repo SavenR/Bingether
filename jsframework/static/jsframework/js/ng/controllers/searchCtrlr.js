35a87f27ddaf8e9bbf2cbf06f55c63d5155f46c3
Saven Roybal <SavenR@users.noreply.github.com>
2015-06-12 19:57:28 -0700

    Connected Search page to guidebox API. Completes #4

--- jsframework/static/jsframework/js/ng/controllers/searchCtrlr.js
+++ jsframework/static/jsframework/js/ng/controllers/searchCtrlr.js
@@ -1,4 +1,37 @@
 app.controller('searchC', function($http){
-    var searchC = this;
+    var self = this;
+
+    // self.battleCry= "WE'RE HERE!!!";
+    self.lastTitle = "";
+
+    self.searchGB = function(){
+        if (!self.showTitle || self.showTitle === self.lastTitle){
+            return
+        };
+
+        self.lastTitle = self.showTitle;
+        self.titleTE = self.showTitle.split(' ').join('%252520');
+
+        // Complete the AJAX request and store the return packet
+        var guideBoxPromise = $http.get(
+        'https://api-public.guidebox.com/v1.43/us/rKz5kRcTCXufMqGHHupHnNBtX6XfTNgI/search/title/' + self.titleTE + '/fuzzy'
+        );
+
+        // Use the success method to extract the data from the payload
+        guideBoxPromise.success( function( data, status, headers, config ){
+            self.shows = data;
+            console.log(data);
+        } );
+
+        // Use the
+        guideBoxPromise.error( function( data, status, headers, config ){
+            console.log( 'status: ' );
+            console.log( status );
+            console.log( data.error );
+        } );
+
+
+
+    }
 
 });
