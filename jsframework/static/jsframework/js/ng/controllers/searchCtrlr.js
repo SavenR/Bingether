7dd4e102e28d7fdc35f79f8dca6bc52e897af059
Saven Roybal <SavenR@users.noreply.github.com>
2015-06-15 20:00:28 -0700

    Pulling overiview information and parsing start year from GuideBox API. Closes #56

--- jsframework/static/jsframework/js/ng/controllers/searchCtrlr.js
+++ jsframework/static/jsframework/js/ng/controllers/searchCtrlr.js
@@ -1,5 +1,8 @@
 app.controller('searchC', function($http){
-    var self = this;
+    var self = this,
+        baseUrl = 'https://api-public.guidebox.com/v1.43/us/rKz5kRcTCXufMqGHHupHnNBtX6XfTNgI/';
+    // debugging, deleteMe
+    self.showTitle = "Pokemon";
 
     // self.battleCry= "WE'RE HERE!!!";
     self.lastTitle = "";
@@ -13,25 +16,33 @@ app.controller('searchC', function($http){
         self.titleTE = self.showTitle.split(' ').join('%252520');
 
         // Complete the AJAX request and store the return packet
-        var guideBoxPromise = $http.get(
-        'https://api-public.guidebox.com/v1.43/us/rKz5kRcTCXufMqGHHupHnNBtX6XfTNgI/search/title/' + self.titleTE + '/fuzzy'
-        );
+        $http.get( baseUrl + 'search/title/' + self.titleTE + '/fuzzy'
+        )
+        .then( function( response ){
+            self.shows = response.data.results.sort(function(a,b){
+                if (a.title.length < b.title.length) { return -1; }; return 1;
+            });
 
-        // Use the success method to extract the data from the payload
-        guideBoxPromise.success( function( data, status, headers, config ){
-            self.shows = data;
-            console.log(data);
-        } );
-
-        // Use the
-        guideBoxPromise.error( function( data, status, headers, config ){
-            console.log( 'status: ' );
-            console.log( status );
-            console.log( data.error );
-        } );
-
-
-
+        },
+        function( response ){
+            console.log( 'An error has occurred: ' );
+            console.log( response.status );
+            console.log( response.statusText );
+            console.log( response.data.error );
+        })
+        .then( function(){
+            angular.forEach( self.shows, function( item, place ){
+                $http.get( baseUrl + 'show/' + item.id )
+                .then(function( response ){
+                    // console.log( response.data );
+                    // console.log( self.shows[place] );
+                    self.shows[place]['overview'] = response.data.overview;
+                    self.shows[place]['year'] = response.data.first_aired.substring(0, 4);
+                })
+                ;
+            } )
+         })
+        ;
     }
 
 });
