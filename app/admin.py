f7c59233e20029450460b4c33b3682f78305cded
Saven Roybal <SavenR@users.noreply.github.com>
2015-06-19 16:57:00 -0700

    Created PERSONAL BINGE data model. Closes #7.

--- app/admin.py
+++ app/admin.py
@@ -1,3 +1,5 @@
 from django.contrib import admin
+from .models import personalBinge
 
 # Register your models here.
+admin.site.register(personalBinge)
--- app/models.py
+++ app/models.py
@@ -1,3 +1,15 @@
+from django.contrib.auth.models import User
 from django.db import models
 
 # Create your models here.
+class personalBinge(models.Model):
+    user = models.ForeignKey(User)
+    showID = models.PositiveIntegerField()
+    showName = models.CharField(max_length=255)
+    showYear = models.PositiveSmallIntegerField()
+    showSummary = models.TextField(blank=True)
+    showImage = models.CharField(max_length=255)
+    active = models.BooleanField(default=True)
+
+    def __unicode__(self):
+        return self.user + ' - ' + self.showName
