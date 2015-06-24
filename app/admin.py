dd01a30b6f5cf929d0c5120eb2b4007a3d6fc320
Saven Roybal <SavenR@users.noreply.github.com>
2015-06-24 11:16:10 -0700

    Registerred Comments to admin page for testing.

--- app/admin.py
+++ app/admin.py
@@ -1,7 +1,9 @@
 from django.contrib import admin
 from .models import personalBinge
 from .models import Bingether
+from .models import Comment
 
 # Register your models here.
 admin.site.register(personalBinge)
 admin.site.register(Bingether)
+admin.site.register(Comment)
