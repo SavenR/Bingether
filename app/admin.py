20cfe394548a9aff6f94fe148d674f56f82bcd83
Saven Roybal <SavenR@users.noreply.github.com>
2015-06-24 01:02:42 -0700

    Registerred Bingethers model. Closes #132

--- app/admin.py
+++ app/admin.py
@@ -1,5 +1,7 @@
 from django.contrib import admin
 from .models import personalBinge
+from .models import Bingether
 
 # Register your models here.
 admin.site.register(personalBinge)
+admin.site.register(Bingether)
