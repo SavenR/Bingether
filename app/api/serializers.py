37f485c59548234338bd788f75e2e5b5c673d45d
Saven Roybal <SavenR@users.noreply.github.com>
2015-06-24 01:26:18 -0700

    Created Bingether Serializer

--- app/api/serializers.py
+++ app/api/serializers.py
@@ -1,10 +1,28 @@
 from rest_framework import serializers
 from app.models import personalBinge
+from app.models import Bingether
 
 # Personal Binges Serializer
 class pbS(serializers.ModelSerializer):
     class Meta:
         model = personalBinge
         fields = ('user', 'showID', 'showName', 'showYear', 'showSummary', 'showImage', 'active')
+
+# Bingethers Serializer
+class btS(serializers.ModelSerializer):
+    class Meta:
+        model = Bingether
+        fields = (
+            'cID',
+            'cUName',
+            'showID',
+            'showName',
+            'showYear',
+            'showSummary',
+            'showImage',
+            'active',
+            'dateCreated',
+            'location',
+            'notes')
 
 
