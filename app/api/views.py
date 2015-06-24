a51372a0f59abcc5b06fa883c1b7f2b428656449
Saven Roybal <SavenR@users.noreply.github.com>
2015-06-24 01:32:55 -0700

    Added All Active Bingethers endpoint. Closes #134

--- app/api/views.py
+++ app/api/views.py
@@ -1,6 +1,8 @@
 from rest_framework import viewsets
 from app.api.serializers import pbS
+from app.api.serializers import btS
 from app.models import personalBinge
+from app.models import Bingether
 from rest_framework import generics
 
 class PBViews(viewsets.ModelViewSet):
@@ -14,5 +16,11 @@ class UsersPBs2 ( generics.ListAPIView ):
     def get_queryset(self):
         cUser = self.request.user
         return personalBinge.objects.filter( user = cUser)
+
+class allActiveBingethers ( generics.ListAPIView ):
+    serializer_class=btS
+
+    def get_queryset(self):
+        return Bingether.objects.filter( active = True )
 
 
--- app/urls.py
+++ app/urls.py
@@ -1,6 +1,6 @@
 from django.conf.urls import url, include
 from rest_framework import routers
-from .api.views import UsersPBs2, PBViews
+from .api.views import UsersPBs2, PBViews, allActiveBingethers
 
 
 router = routers.DefaultRouter()
@@ -9,5 +9,6 @@ router.register(r'pbu', PBViews, base_name='pbu')
 urlpatterns = [
     url(r'^', include(router.urls)), # Include router urls into our urlpatterns
     url(r'^api-auth/', include('rest_framework.urls', namespace='rest_framework')),
-    url(r'^users/', UsersPBs2.as_view(), name='userList')
+    url(r'^users/', UsersPBs2.as_view(), name='userList'),
+    url(r'^bingethers/', allActiveBingethers.as_view(), name='activeBRs'),
 ]
