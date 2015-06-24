052c60e793c1ec38f065169a278719beaffc2249
Saven Roybal <SavenR@users.noreply.github.com>
2015-06-24 09:32:42 -0700

    Connected Community page to endpoint and copied Personal Binges layout for the moment.

--- jsframework/static/jsframework/js/ng/controllers/communityBRsCtrlr.js
+++ jsframework/static/jsframework/js/ng/controllers/communityBRsCtrlr.js
@@ -1,82 +1,21 @@
 app.controller('communityBRsC', function($http){
 var self = this,
-        baseUrl = '/api/app/users/';
-    // debugging, deleteMe
+    url = '/api/app/bingethers/';
 
-    self.pbs = [1, 2, 3];
-    self.tempShow = 'http://www.gif.tv/tv.png'
+    // API call for all active Bingers
+    self.getCommunityBRs = function(){
+        $http.get( url )
+        .then( function( response ){
+            self.pbs = response.data;
+        },
+        // Error behavior
+        function( response ){
+            console.log( 'An error has occurred: ' );
+            console.log( response.status );
+            console.log( response.statusText );
+            console.log( response.data.error );
+        });
+    };
 
-    // self.searchGB = function(){
-    //     // Blocks accidental blank/duplicate searches
-    //     if (!self.showTitle || self.showTitle === self.lastTitle){
-    //         return
-    //     };
-
-    //     self.shows = [];
-    //     self.lastTitle = self.showTitle;
-    //     self.searchStatement = 'Searching...';
-
-    //     // Cleaning title for GuideBox API
-    //     self.titleTE = self.showTitle.split(' ').join('%252520');
-
-    //     // Search GuideBox API using the cleaned title
-    //     $http.get( baseUrl + 'search/title/' + self.titleTE + '/fuzzy')
-    //     .then( function( response ){
-
-    //         // Iterating through found shows
-    //         response.data.results.map(function( item, place, input ){
-
-    //             // Accesses additional show information from GuideBox
-    //             $http.get( baseUrl + 'show/' + item.id )
-    //             .then(function( response ){
-
-    //                 // Checks if item is a television show
-    //                 if ( response.data.type === "television" ){
-
-    //                     // Adds television show to view
-    //                     self.shows.push(item);
-
-    //                     // Adds additional information to display
-    //                     var lastIndex = self.shows.length - 1;
-    //                     self.shows[lastIndex]['overview'] = response.data.overview;
-    //                     // (first_aired can === false... )
-    //                     self.shows[lastIndex]['year'] = (parseInt(response.data.first_aired) || null);
-
-    //                     // Sorts shows based on title length
-    //                     self.shows.sort(function(a,b){
-    //                         if (a.title.length < b.title.length) { return -1; }
-    //                         return 1;
-    //                     });
-
-    //                     // Generates search Statement
-    //                     self.searchStatement =
-    //                         (lastIndex+1) + ' Search Result' +
-    //                         ( (lastIndex+1) === 1 ? ' ' : 's ' )  +
-    //                         'for ' + self.showTitle + ':';
-    //                 };
-    //             });
-    //         });
-
-    //         // Displays 0 results message
-    //         if ( !self.shows[0] ) {
-    //             self.searchStatement = '0 Seach Results for ' + self.showTitle + '.';
-    //         }
-    //     },
-
-    //     // Error behavior
-    //     function( response ){
-    //         console.log( 'An error has occurred: ' );
-    //         console.log( response.status );
-    //         console.log( response.statusText );
-    //         console.log( response.data.error );
-    //     });
-    // }; //Closes self.searchGB
-
-    // self.addToList = function( elem ){
-    //     console.log( elem );
-    // }
-
-    // self.createBR = function( elem ){
-    //     console.log( elem );
-    // }
+    self.getCommunityBRs()
 });
